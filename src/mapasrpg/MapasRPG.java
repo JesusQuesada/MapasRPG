24508f73560fb645f53fbb75bead21c93d1adb32
Nati <Nati@192.168.0.20>
2017-03-02 22:30:35 +0100

    0.1

---
+++ src/mapasrpg/MapasRPG.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mapasrpg;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author JesusQuesada
+ */
+public class MapasRPG {
+    String direccion;
+    int nSector,nEventoAgresion, nEvento;
+    Scanner sc = new Scanner(System.in);
+
+    public MapasRPG() {
+    }
+    
+    
+    
+    public  void comprobarCardinales(String direccion){
+        
+    
+
+    }
+    
+}
+    
+    
+
